f746f9cf7be307a7caf648c7bd935404a25eeff8
Axel Cardin <thenebiroschaine@gmail.com>
2026-05-23 13:22:02 +0200

    icons

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-const CACHE_NAME = 'rule34-lesbian-v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
+const CACHE_NAME = 'rule34-lesbian-v2';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
